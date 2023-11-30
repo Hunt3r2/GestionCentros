dbeafd243b522ec39050f949c2da5f80b9f281a1
david <david.lejo@isepceu.es>
2023-11-30 18:36:25 +0100

    Añado la clase Alumno
    30/11/23

---
+++ src/Alumno.java
@@ -0,0 +1,18 @@
+
+public class Alumno {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	String ID ;
+	String nombre;
+	String apellido;
+	String apellido2;
+	String email;
+	String tlf;
+	String ni;
+	String numeroID[] = new String[] {"0","1","2","3","4"};
+
+}
